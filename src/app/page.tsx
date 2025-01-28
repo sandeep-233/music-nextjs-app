607c5d4475f5dbd01619695e99ecc073f3ec43e0
SANDEEP KUMAR <sandeepkv233@gmail.com>
2025-01-28 14:26:37 +0530

    added UpcomingWebinars component with card-hover-effect using Aceternity UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { FeaturedCourses } from "@/components/FeaturedCourses";
 import { HeroSection } from "@/components/HeroSection";
 import { MusicSchoolTestimonials } from "@/components/MusicSchoolTestimonials";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import { WhyChooseUs } from "@/components/WhyChooseUs";
 
 
@@ -11,6 +12,7 @@ export default function Home() {
       <FeaturedCourses/>
       <WhyChooseUs/>
       <MusicSchoolTestimonials/>
+      <UpcomingWebinars/>
     </main>
   );
 }
